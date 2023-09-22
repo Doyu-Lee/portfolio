33e7e2a116b80f69de2ae0f9ed0531555d4c3cb3
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-09-22 22:56:37 +0900

    chore: 라우터 폴더구조 추가 외

---
+++ src/app/roadmap/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function RoadMap() {
+  return (
+    <div className={styles.container}>
+      <h2>RoadMap</h2>
+    </div>
+  );
+}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,21 +6,21 @@ const Header = () => {
   const navMenus = ['roadmap', 'dots'];
 
   return (
-    <nav className={styles['header-nav']}>
+    <header className={styles['header-nav']}>
       <div className={styles['nav-box']}>
         <div className={styles['logo-box']}>
           <Logo />
         </div>
-        <div className={styles['menu-box']}>
+        <nav className={styles['menu-box']}>
           {navMenus.map((menu) => (
             <Link data-text={menu} href={`/${menu}`} key={menu} className={styles.menu}>
               <span className={styles.highlight} />
               {menu}
             </Link>
           ))}
-        </div>
+        </nav>
       </div>
-    </nav>
+    </header>
   );
 };
 
