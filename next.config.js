d3d383361b2be410405aaf7d187675709665115c
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-09-22 20:30:40 +0900

    chore: gsap 설치 및 scss 세팅 수정

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const path = require('path');
 
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "_variables", "_mixins";`,
+    includePaths: [path.join(__dirname, 'style')],
   },
 };
 
