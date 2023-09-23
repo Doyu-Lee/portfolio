e676bf075aae04c7bb543705e404b3e979f6b19b
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-09-23 19:04:09 +0900

    design: 헤더에 한/영 변환에 따른 폰트 개별 적용

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,28 @@
 'use client';
 
 import Link from 'next/link';
+import { useTranslation } from '@/app/i18n/client';
 import { navMenus } from '@/constants/navMenus';
 import Logo from '../common/logo/Logo';
 import styles from './Header.module.scss';
 
-const Header = () => {
+const Header = ({ lng }: { lng: string }) => {
+  const { t } = useTranslation(lng, 'header');
   return (
     <header className={styles['header-nav']}>
       <div className={styles['nav-box']}>
-        <Logo />
+        <Logo lng={lng} />
         <div className={styles.align}>
-          <nav className={styles['menu-box']}>
+          <nav className={`${styles['menu-box']} ${lng === 'ko' && styles.ko}`}>
             {navMenus.map((menu) => (
-              <Link data-text={menu} href={`/${menu}`} key={menu} className={styles.menu}>
+              <Link
+                data-text={menu}
+                href={`/${lng}/${menu}`}
+                key={menu}
+                className={styles.menu}
+              >
                 <span className={styles.highlight} />
-                {menu}
+                {t(menu)}
               </Link>
             ))}
           </nav>
