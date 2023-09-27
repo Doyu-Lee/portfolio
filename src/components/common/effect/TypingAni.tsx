87a8afd71e4688b6ff600c785a161595a5944cfb
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-09-28 00:31:16 +0900

    fix: react 18 호환을 위해 타이핑 효과 애니메이션 라이브러리 교체

--- src/components/common/effect/TypingAni.tsx
+++ src/components/common/effect/TypingAni.tsx
@@ -1,28 +1,29 @@
-/* eslint-disable prettier/prettier */
-import Typist from 'react-typist';
-import 'react-typist/dist/Typist.css';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import { useTranslation } from '@/app/i18n/client';
 import { LngProps } from '@/types/lngSwitch';
 import { useAfterSeconds } from '@/hooks/useAfterSeconds';
 import styles from './TypingAni.module.scss';
 
-export default function TypingAni({ lng  }:LngProps) {
+export default function TypingAni({ lng }: LngProps) {
   const { t } = useTranslation(lng, 'contacts');
   const isMounting = useAfterSeconds(1000);
 
+  const [text] = useTypewriter({
+    words: ['', t('subTitle1'), t('subTitle2'), t('subTitle3')],
+  });
+
   return (
     <div className={`${styles.box} ${lng === 'ko' && styles.ko}`}>
-      { lng === 'en' && t('subTitle4')}
+      {lng === 'en' && t('subTitle4')}
       <div className={styles.point}>
-        {isMounting && <Typist className="inline" startDelay={1000}>
-          {t('subTitle1')}
-          <Typist.Backspace count={lng === 'ko' ? 18 : 35} delay={2000} />
-          {t('subTitle2')}
-          <Typist.Backspace count={lng === 'ko' ? 18 : 45} delay={2000} />
-          {t('subTitle3')}
-        </Typist>}
+        {isMounting && (
+          <div>
+            {text}
+            <Cursor cursorColor="green" />
+          </div>
+        )}
       </div>
-      { lng === 'ko' && t('subTitle4')}
+      {lng === 'ko' && t('subTitle4')}
     </div>
   );
 }
