c78f25c6f404f477bbf9e6deb13779d8468a4648
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-10-12 17:16:12 +0900

    chore: CSS 디테일 조정 외 reset 파일 이동

--- src/components/common/buttons/Button1.tsx
+++ src/components/common/buttons/Button1.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { ReactNode } from 'react';
 import { getProps } from '@/utils/common/getButtonProps';
 import styles from './Button1.module.scss';
@@ -8,15 +9,39 @@ interface Button1Props {
   handler?: () => void;
   title?: string;
   children?: ReactNode;
+  isLink?: string;
+  toWhere?: string;
+  isNewTab?: boolean;
 }
 
-const Button1 = ({ handler, title, children }: Button1Props) => {
-  const buttonContents = getProps(title, children);
+const Button1 = ({
+  handler,
+  title,
+  children,
+  isLink,
+  toWhere,
+  isNewTab,
+}: Button1Props) => {
+  const contents = getProps(title, children);
 
+  if (isLink && toWhere) {
+    return (
+      <div className={styles['button-box']}>
+        <Link
+          href={toWhere}
+          className={styles.button}
+          target={isNewTab ? '_blank' : undefined}
+          rel={isNewTab ? 'noopener' : undefined}
+        >
+          {contents}
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className={styles['button-box']}>
       <button type="button" className={styles.button} onClick={handler}>
-        {buttonContents}
+        {contents}
       </button>
     </div>
   );
--- src/components/common/effect/card/Card.tsx
+++ src/components/common/effect/card/Card.tsx
@@ -31,7 +31,6 @@ const Card3D = ({ lng }: LngProps) => {
           <div className={styles.back} />
         </>
       )}
-      ;
     </div>
   );
 };
