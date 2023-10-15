05bbf8dea7aa0843ab67c75ec7b6a68e60fd0f81
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-10-16 03:24:44 +0900

    feat: 한/영 모드에 따른 메타태그 정보 개별 출력

---
+++ src/constants/SEO/home.ts
@@ -0,0 +1,49 @@
+export const homeMetaData = {
+  metadataKO: {
+    title: {
+      template: '🐱 %s | 도유의 블로그',
+      default: '🐱 도유의 블로그',
+    },
+    description: '개발과 관련된 다양한 정보들을 기록하는 장소입니다.',
+    icons: {
+      icon: '/images/meta/favicon.ico',
+    },
+    openGraph: {
+      title: {
+        template: '🐱 %s | 도유의 블로그',
+        default: '🐱 도유의 블로그',
+      },
+      description: '개발과 관련된 다양한 정보들을 기록하는 장소입니다.',
+      images: [
+        {
+          url: '/images/meta/opengraph-image.png',
+          alt: '도유의 블로그 OG 이미지',
+        },
+      ],
+    },
+  },
+
+  metadataEN: {
+    title: {
+      template: "🐱 %s | Doyu's Blog",
+      default: "🐱 Doyu's Blog",
+    },
+    description: 'A blog recording various insights related to development',
+    icons: {
+      icon: '/images/meta/favicon.ico',
+    },
+    openGraph: {
+      title: {
+        template: "🐱 %s | Doyu's Blog",
+        default: "🐱 Doyu's Blog",
+      },
+      description: 'A blog recording various insights related to development',
+      images: [
+        {
+          url: '/images/meta/opengraph-image.png',
+          alt: "Doyu's Blog OG Image",
+        },
+      ],
+    },
+  },
+};
