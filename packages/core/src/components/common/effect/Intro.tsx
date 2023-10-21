2940e2aa45f5edfd6314c0ff9045fd66aef63416
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-10-21 15:13:13 +0900

    design(core): 공통 css 클래스 디자인 추가

--- packages/core/src/components/common/effect/Intro.tsx
+++ packages/core/src/components/common/effect/Intro.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-key */
+
 'use client';
 
 import { Trans } from 'react-i18next';
 import { useTranslation } from '@/app/i18n/client';
+import commonStyles from '@/styles/base/common.module.scss';
 import { LngProps } from '@/types/lngSwitch';
 import { LetterDomino } from '@/hooks/useLetterDomino';
 import GuideBox from '../Guide';
@@ -17,27 +20,30 @@ export default function Intro({ lng }: LngProps) {
     <div className={styles.wrap}>
       <div className={styles.content}>
         <div className={styles.align}>
-          <div className={`${styles.drift} ${lng === 'en' && styles.en1}`}>
+          <div className={`${styles.drift} ${lng === 'en' ? styles.en1 : ''}`}>
             {childSpans}
           </div>
         </div>
-        <div className={`${styles.introduction} ${lng === 'en' && styles.en2}`}>
-          <Trans key="강조" components={[<span key="point" className={styles.point} />]}>
+        <div className={`${styles.introduction} ${lng === 'en' ? styles.en2 : ''}`}>
+          <Trans
+            i18nKey="introduction1"
+            components={[<span className={commonStyles.highlights} />]}
+          >
             {t('introduction1')}
           </Trans>
         </div>
-        <div className={`${styles.introduction} ${lng === 'en' && styles.en2}`}>
+        <div className={`${styles.introduction} ${lng === 'en' ? styles.en2 : ''}`}>
           <Trans
-            key="강조"
+            i18nKey="introduction2"
             components={[
-              <span key="weight" className={styles.weight} />,
-              <span key="highlight" className={styles.highlight} />,
+              <span className={commonStyles.points} />,
+              <span className={commonStyles.italics} />,
             ]}
           >
             {t('introduction2')}
           </Trans>
         </div>
-        <GuideBox text={t('guide')} lng={lng} />
+        <GuideBox text={t('guide')} hidedText={t('hidedGuide')} lng={lng} />
       </div>
     </div>
   );
