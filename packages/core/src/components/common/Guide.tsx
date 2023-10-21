0c5c8e1b3b05c559f5fd63b341defe6d997524f6
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-10-21 15:14:07 +0900

    feat(core): Guide 컴포넌트에 선택적 토글 기능 추가

--- packages/core/src/components/common/Guide.tsx
+++ packages/core/src/components/common/Guide.tsx
@@ -1,27 +1,43 @@
+/* eslint-disable react/jsx-key */
+
 'use client';
 
+import { useState } from 'react';
 import { Trans } from 'react-i18next';
+import commonStyles from '@/styles/base/common.module.scss';
 import { LngProps } from '@/types/lngSwitch';
+import Button1 from './buttons/Button1';
 import styles from './Guide.module.scss';
 
 interface GuideBoxProps extends LngProps {
   text: string;
+  hidedText?: string;
 }
 
-export default function GuideBox({ lng, text }: GuideBoxProps) {
+export default function GuideBox({ lng, text, hidedText }: GuideBoxProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOnClick = () => setIsOpen((prev) => !prev);
+  const transComponents = [
+    <span className={commonStyles.italics} />,
+    <span className={commonStyles.points} />,
+    <span className={commonStyles.weights} />,
+    <span className={commonStyles.highlights} />,
+    <div className={commonStyles.margins} />,
+    <Button1 handler={handleOnClick} />,
+  ];
+
   return (
-    <div className={`${styles.guide} ${lng === 'en' && styles.en}`}>
+    <div
+      className={`${styles.guide} ${lng === 'en' && styles.en} ${isOpen && styles.open}`}
+    >
       <div>
-        <Trans
-          key="맞춤형"
-          components={[
-            <span key="italic" className={styles.italic} />,
-            <span key="point" className={styles.point} />,
-            <span key="weight" className={styles.weight} />,
-            <span key="highlight" className={styles.highlight} />,
-          ]}
-        >
+        <Trans i18nKey="맞춤형" components={transComponents}>
           {text}
+        </Trans>
+      </div>
+      <div className={`${commonStyles.toggles} ${isOpen ? styles.open : styles.close}`}>
+        <Trans i18nKey="맞춤형" components={transComponents}>
+          {hidedText}
         </Trans>
       </div>
     </div>
